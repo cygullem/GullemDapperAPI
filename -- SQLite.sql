139e3a67f594fff09ef255ad5ccc5af9ba71a2c7
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-01 20:26:17 +0800

    Added C.O.R.S.

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,17 +1,16 @@
 -- SQLite
-CREATE TABLE Client
-(
-    ID integer PRIMARY KEY,
-    ClientName text null,
-    Residency text null
-);
+-- CREATE TABLE Client
+-- (
+--     ID integer PRIMARY KEY,
+--     ClientName text null,
+--     Residency text null
+-- );
 
-INSERT INTO Client
-    (ClientName, Residency)
-VALUES
-    ('John Nico', 'Bungtod'),
-    ('John Dhaniel', 'Pelaez'),
-    ('John Brent', 'Taytayan')
+-- INSERT INTO Client
+--     (ClientName, Residency)
+-- VALUES
+--     ('John Nico', 'Bungtod'),
+--     ('John Dhaniel', 'Pelaez'),
+--     ('John Brent', 'Taytayan')
 
-SELECT *
-FROM Client
+SELECT * FROM Client    
--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(option => {
+    option.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseCors();
 app.Run();
